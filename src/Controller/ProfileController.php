24bdcb03ed8ffcbf026624f7cbf9f07ae24a5002
PerrinGaetan <perringaetandu31@gmail.com>
2021-11-26 03:46:14 +0100

    start feature to store album's informations on db

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,12 +2,21 @@
 
 namespace App\Controller;
 
+use App\Model\AddToListModel;
 use App\Model\SearchManager;
 
 class ProfileController extends AbstractController
 {
     public function displayListById()
     {
+        $data = array_map('trim', $_POST);
+        $data = array_map('htmlentities', $_POST);
+        $addMyList = new AddToListModel();
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_POST['artist'] !== "") {
+            $data[] = $_SESSION['userId'];
+            $addMyList->addToMyList($data);
+        }
+
         if (!empty($_GET['id'])) {
             $_GET['id'] = intval(trim(htmlentities($_GET['id'])));
             $api = new ApiController();
@@ -17,11 +26,16 @@ class ProfileController extends AbstractController
             foreach ($lists as $list) {
                 $informations[] = $api->index($list);
             }
-
-            // var_dump($lists, $informations);
-            // die();
             return $this->twig->render('Home/profile.html.twig', ['results' => $informations]);
         }
+
+        var_dump($data);
+        // die();
+
         return $this->twig->render('Home/profile.html.twig');
     }
+
+    // public function createList()
+    // {
+    // }
 }
