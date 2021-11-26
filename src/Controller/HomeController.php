227ef82e84b3efccb9dbbe6eb68c75170720736d
Benjamin Deneux <benjamindeneux@hotmail.com>
2021-11-26 09:46:45 +0100

    style homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,6 +9,8 @@
 
 namespace App\Controller;
 
+use App\Model\HomepageManager;
+use Symfony\Component\HttpClient\HttpClient;
 
 class HomeController extends AbstractController
 {
@@ -22,8 +24,15 @@ class HomeController extends AbstractController
      */
     public function index()
     {
-        return $this->twig->render('Home/homepage.html.twig');
+        $client = HttpClient::create();
+        $nirvana = $client->request('GET', 'https://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=704b3bb51d87a99023fabedf451720ca&artist=skalpel&album=skalpel&format=json');
+        $result = $nirvana->toArray();
+        $homeModel = new HomepageManager;
+        $allUsers = $homeModel->selectAllByUsers();
+        return $this->twig->render('Home/homepage.html.twig', ['result' => $result, 'users' => $allUsers]);
     }
+
+
     public function show()
     {
         return $this->twig->render('Home/profile.html.twig');
